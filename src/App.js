c5f07826d0fdbb246a1b5a64b27144cb001d4283
djsmoothy <djsmoothy1@hotmail.com>
2020-11-20 12:53:21 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Componets/Home'
+import AllScans from './Componets/scans/AllScans'
+import Scanresults from './Componets/scans/Scanresults'
+import Createscan from './Componets/scans/Createscan'
+import Allprojects from './Componets/projects/Allprojects'
+import Projectresult from './Componets/projects/Projectresult'
+import Createprojects from './Componets/projects/Createprojects'
+import Editproject from './Componets/projects/Editproject'
+import Editscan  from './Componets/scans/Editscan'
+import About  from './Componets/About'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/allscans" component={AllScans}  />
+        <Route path="/about" component={About}  />
+        <Route path="/scanresult/:id" component={Scanresults}  />
+        <Route path="/create-scan" component={Createscan}  />
+        <Route path="/all-project" component={Allprojects}  />
+        <Route path="/create-project" component={Createprojects}  />
+        <Route path="/edit-project/:id" component={Editproject}  />
+        <Route path="/edit-scan/:id" component={Editscan}  />
+        <Route path="/project-result/:id" component={Projectresult}  />
+      
+    </Switch>
+</main>
   );
 }
 
